38ffdd549713e3ebc1d0976487e32e95f8677a4c
Rafael_Garcia <pmsp.smti.dev@hotmail.com>
2020-12-09 13:26:50 -0300

    simple xml reader

---
+++ xmlReader.py
@@ -0,0 +1,24 @@
+import xml.etree.ElementTree as ET
+
+input = '''<outer>
+  <users>
+    <user x="0">
+      <id>001</id>
+      <name>Nargaroth</name>
+    </user>
+    <user x="1">
+      <id>002</id>
+      <name>Morgoth</name>
+    </user>
+  </users>
+</outer>'''
+
+users = ET.fromstring(input)
+lst = users.findall('users/user')
+
+print('User count:', len(lst))
+
+for item in lst:
+  print("name:", item.find('name').text)
+  print("id:", item.find('id').text)
+  print("Attributes", item.get("x"))
